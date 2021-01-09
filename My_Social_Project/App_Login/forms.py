2a8a8b565306659670dd982bd8f45ca50e4cb34a
ztarikul <ztarikulislam@gmail.com>
2021-01-10 04:56:06 +0600

    Project Upload

---
+++ My_Social_Project/App_Login/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from App_Login.models import UserProfile
+
+class CreateNewUser(UserCreationForm):
+    username = forms.CharField(required=True, label="", widget=forms.TextInput(attrs={'placeholder':'Username'}))
+    email = forms.EmailField(required=True, label="", widget=forms.TextInput(attrs={'placeholder':'Email'}))
+    password1 = forms.CharField(required=True, label="", widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+    password2 = forms.CharField(required=True, label="", widget=forms.PasswordInput(attrs={'placeholder':'Confirm Password'}))
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class EditProfile(forms.ModelForm):
+    dob = forms.DateField(widget=forms.TextInput(attrs={'type':'date'}))
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
